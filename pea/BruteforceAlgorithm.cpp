adc1e57a6cd31fb5927f06ee54dc78d6be282ce7
krzemien888 <krzeminski.k@outlook.com>
2017-11-12 15:10:25 +0100

    Fixed bruteforce

--- pea/BruteforceAlgorithm.cpp
+++ pea/BruteforceAlgorithm.cpp
@@ -11,11 +11,15 @@ Result BruteforceAlgorithm::apply(matrixGraph * graph)
 	std::chrono::high_resolution_clock::time_point startTime;
 	std::chrono::high_resolution_clock::time_point endTime;
 
-
+	std::vector<int> dbgV = { 0,1,3,2 };
 
 	startTime = std::chrono::high_resolution_clock::now();
 	while (std::next_permutation(currPermutation.begin(), currPermutation.end()))
 	{
+		if (currPermutation == dbgV)
+			;
+
+
 		if (isBetter(bestPermutation, currPermutation, graph))
 			bestPermutation = currPermutation;
 	}
@@ -27,7 +31,7 @@ Result BruteforceAlgorithm::apply(matrixGraph * graph)
 	output.result = calculateCost(bestPermutation, graph);
 	output.time = (int)std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
 
-	return Result();
+	return output;
 }
 
 bool BruteforceAlgorithm::isBetter(std::vector<int>& best, std::vector<int>& curr, matrixGraph * graph)
@@ -35,7 +39,7 @@ bool BruteforceAlgorithm::isBetter(std::vector<int>& best, std::vector<int>& cur
 	if (best.size() != curr.size())
 		throw std::invalid_argument("BruteforceAlgorithm::isBetter: Not equal vectors given");
 
-	return calculateCost(best, graph) < calculateCost(curr, graph);
+	return calculateCost(best, graph) > calculateCost(curr, graph);
 }
 
 long int BruteforceAlgorithm::calculateCost(std::vector<int>& permutation, matrixGraph * graph)
@@ -43,7 +47,7 @@ long int BruteforceAlgorithm::calculateCost(std::vector<int>& permutation, matri
 	long int result = 0;
 	for (size_t i = 0; i < permutation.size(); i++)
 		result += graph->getConnectionValue(permutation[i],
-			(i+1 == permutation.size()) ? permutation[i + 1] : permutation[0]);
+			(i+1 != permutation.size()) ? permutation[i + 1] : permutation[0]);
 
 	return result;
 }
--- peaTests/BruteforceAlgorithmTests.cpp
+++ peaTests/BruteforceAlgorithmTests.cpp
@@ -6,10 +6,16 @@ TEST(BruteforceAlgorithmTests, ShouldProperlySolveGraph)
 	BruteforceAlgorithm alg;
 
 	std::vector<std::vector<int>> v = {
-		{},
-		{},
-		{},
-		{},
-		{}
+		{ 0,1,15,6 },
+		{ 2,0,7,3 },
+		{ 9,6,0,12 },
+		{ 10,4,8,0 }
 	};
+	matrixGraph graph;
+	graph.setMatrix(v);
+
+	auto result = alg.apply(&graph);
+	std::vector<int> rightPath = { 0,1,3,2 };
+	ASSERT_EQ(result.path, rightPath);
+	ASSERT_EQ(result.result, 21);
 }
