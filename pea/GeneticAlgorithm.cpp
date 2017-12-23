03af639c00b903635b466654998b1a41cebc3590
krzemien888 <krzeminski.k@outlook.com>
2017-12-23 13:13:10 +0100

    GA template created

--- pea/GeneticAlgorithm.cpp
+++ pea/GeneticAlgorithm.cpp
@@ -4,10 +4,51 @@
 
 Result GeneticAlgorithm::apply(matrixGraph * graph)
 {
-	return Result();
+	initPopulation();
+	int generationCount = 0;
+	int generationsWithoutImprovement = 0;
+	std::chrono::high_resolution_clock::time_point startTime;
+	std::chrono::high_resolution_clock::time_point endTime;
+
+
+	Individual bestIndividual = m_population.getFittest();
+
+	startTime = std::chrono::high_resolution_clock::now();
+	while (verifyEndingCondition(generationCount, generationsWithoutImprovement))
+	{
+		auto selected = selectParents();
+		auto newPopulation = crossoverPopulation(selected);
+		newPopulation = mutatePopulation(newPopulation);
+
+		m_population = newPopulation;
+			
+		trimPopulation();
+
+		if (bestIndividual > m_population.getFittest())
+			bestIndividual = m_population.getFittest();
+	}
+	endTime = std::chrono::high_resolution_clock::now();
+
+	Result output;
+	output.path = bestIndividual.genotype;
+	output.result = bestIndividual.cost;
+	output.time = (int)std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+	output.fileName = graph->getName;
+
+	return output;
 }
 
 std::string GeneticAlgorithm::toString()
 {
 	return std::string();
+}
+
+bool GeneticAlgorithm::verifyEndingCondition(const int & generationCount, const int & generationWithoutImprovementCount)
+{
+	return generationCount < m_generationLimit && generationWithoutImprovementCount < m_generationsWithoutImprovementLimit;
+}
+
+bool Individual::operator>(const Individual & other) const
+{
+	return cost > other.cost;
 }
--- pea/GeneticAlgorithm.h
+++ pea/GeneticAlgorithm.h
@@ -1,14 +1,81 @@
 #pragma once
+#include "stdafx.h"
 #include "IAlgorithm.h"
-
 
 class GeneticAlgorithm :
 	public IAlgorithm
 {
 public:
+	// Constructors & destructors
 	virtual ~GeneticAlgorithm() = default;
 
+	// Algorithm interface
 	virtual Result apply(matrixGraph* graph) override;
 	virtual std::string toString() override;
+
+	// Setters and getters
+	void setPopulationLimit(const int newPopulationLimit);
+	int getPopulationLimit() const;
+
+	void setGenerationLimit(const int newGenerationLimit);
+	int getGenerationLimit() const;
+
+	void setGenerationsWithoutImprovementLimit(const int newLimit);
+	int getGenerationswithoutImprovementLimit() const;
+
+	void setCrossoverType(const CrossoverType & selectedType);
+	CrossoverType getCrossoverType() const;
+
+	// Population operations
+	Population trimPopulation();
+	Population crossoverPopulation(std::list<std::pair<Individual, Individual>> & selectedParents);
+	Population mutatePopulation(Population & population);
+	std::list<std::pair<Individual, Individual>> selectParents();
+	std::list<Individual> getSortedPopulation();
+
+	// Selection operators
+	std::list<Individual> rouletteSelection();
+
+	// Crossover operators
+	std::pair<Individual, Individual> partialMappedCrossover(Individual & firstParent, Individual & secondParent);
+	std::pair<Individual, Individual> orderCrossover(Individual & firstParent, Individual & secondParent);
+	std::pair<Individual, Individual> cycleCrossover(Individual & firstParent, Individual & secondParent);
+
+	// Mutate operators
+
+private:
+
+	// Properties
+
+	// Algorithm settings
+	int m_populationLimit;
+	int m_generationLimit;
+	int m_generationsWithoutImprovementLimit;
+	CrossoverType m_crossoverType;
+
+	// Inner data properies
+	Population m_population;
+
+	// Initialize methods
+	void initPopulation();
+	std::pair<Individual, Individual> crossover(Individual & firstParent, Individual & secondParent);
+	bool verifyEndingCondition(const int & generationCount, const int & generationWithoutImprovementCount);
 };
 
+
+struct Individual
+{
+	std::vector<int> genotype;
+	int cost;
+	bool operator>(const Individual& other) const;
+};
+
+enum class CrossoverType {
+	PMX, OX, CX
+};
+
+struct Population
+{
+	std::priority_queue<Individual, std::vector<Individual>, std::greater<Individual>> m_populationQueue;
+	Individual getFittest() const;
+};
